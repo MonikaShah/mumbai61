64c7a2edd6b5a2c2674d84e83d88fa411ef2fe26
Monika <monikapatira@gmail.com>
2021-11-16 11:26:52 +0530

    admin.py updated

--- map/admin.py
+++ map/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 
 from .models import Ward61BuildingsOsm2Nov2021
 admin.site.register(Ward61BuildingsOsm2Nov2021)
+class Ward61BuildingsOsm2Nov2021(admin.ModelAdmin):
+    ordering = ['name']
