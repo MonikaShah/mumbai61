292ce392bd02d907b13ead1714134fb58d77b9de
Monika Shah <monikapatira@gmail.com>
2022-10-14 22:08:44 +0530

    report builder added in url

--- mumbai61/urls.py
+++ mumbai61/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+#deprecated next line
 # from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +28,7 @@ urlpatterns = [
     # path('map/',include('map.urls')),
     # path('i18n/', include('django.conf.urls.i18n')),
     re_path(r'^i18n/', include('django.conf.urls.i18n')),
-   
+    # deprecated
+    # url(r'^report_builder/', include('report_builder.urls'))
+   re_path(r'^report_builder/', include('report_builder.urls'))
 ]
