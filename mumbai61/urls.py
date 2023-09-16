e882b94bb04b30fba17453c0e49111754d6eacd1
monikashah <monikapatira@gmail.com>
2023-09-16 11:39:35 +0530

    updated mumbai61 urls

--- mumbai61/urls.py
+++ mumbai61/urls.py
@@ -24,13 +24,28 @@ from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 
+# # django rest api 
+# from rest_framework import routers
+# from home import views
 
+# router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
+
+# # Wire up our API using automatic URL routing.
+# # Additionally, we include login URLs for the browsable API.
+# urlpatterns = [
+#     path('', include(router.urls)),
+#     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+# ]
+# # django api over
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('zerowaste.urls')),
     path('map/',include('map.urls')),
     path('home/', include('home.urls')),
+    path('nowastes_dashboard3/', include('nowastes_dashboard3.urls')),
     # path('map/',include('map.urls')),
     # path('i18n/', include('django.conf.urls.i18n')),
     re_path(r'^i18n/', include('django.conf.urls.i18n')),
@@ -40,6 +55,6 @@ urlpatterns = [
     path('cms/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
     path('pages/', include(wagtail_urls)),
+    path('api/', include("nowasteapi.urls"))
     
-    
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
