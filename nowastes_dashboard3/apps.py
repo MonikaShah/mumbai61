1e67966a6b9cd48165b752dcbc4fcf0002ee9a2d
MonikaShah <monikapatira@gmail.com>
2024-02-27 12:58:53 +0530

    Delete nowastes_dashboard3 directory

--- nowastes_dashboard3/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReactDashboardConfig(AppConfig):
-    name = 'nowastes_dashboard3'
--- nowastes_dashboard3/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import dash3
-from . import views
-
-
-urlpatterns = [
-    path('',dash3,name="nowastes_dashboard3")
-]
